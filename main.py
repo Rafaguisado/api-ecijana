d1360603647ed4f40b7571088b17a021cad4c5c6
Rafael Guisado <rafaguisado@MacBook-Air-de-Rafael.local>
2026-06-17 13:20:21 +0200

    Corregir indentacion endpoint analizar material

--- main.py
+++ main.py
@@ -122,13 +122,17 @@ def top_proveedores(x_api_key: str = Header(None)):
 
     columnas = list(df.columns)
 
-        return {
+    return {
         "columnas_detectadas": columnas,
         "datos": df.head(20).to_dict(orient="records")
     }
 
 
 @app.get("/analizar-material")
+def analizar_material(
+    nombre: str,
+    x_api_key: str = Header(None)
+):
     verificar_api_key(x_api_key)
 
     df = pd.read_excel(EXCEL, sheet_name="api_materiales").fillna("")
